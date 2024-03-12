8c8d4fdf6b5b933eb32647ccb0451cc24e27df72
Mity <tricolorkotovsk1@mail.ru>
2024-03-12 12:41:25 +0300

    добавлено приложение

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, re_path
+import HelloDjangoApp.views
 
 # Uncomment next two lines to enable admin:
 #from django.contrib import admin
 #from django.urls import path
 
 urlpatterns = [
+    re_path(r'^$', HelloDjangoApp.views.index, name='index'),
+    re_path(r'^home$', HelloDjangoApp.views.index, name='home')
     # Uncomment the next line to enable the admin:
     #path('admin/', admin.site.urls)
 ]
